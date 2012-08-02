3b89099fc5c6b3a4fcf2fbdfa16912bec123b488
Todd Barchok <todd@barchok.com>
2012-08-02 16:02:03 -0400

    Added a simple way to get article contents, handling 401 errors as needed

--- src/main/webapp/WEB-INF/api/main.js
+++ src/main/webapp/WEB-INF/api/main.js
@@ -54,6 +54,22 @@ app.get( '/index.html', function ( req ) {
 	return homepage( req );
 } );
 
+app.get('/secure/article', function(req) {
+    return json({
+        title: "SECURE ARTICLE TITLE",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco" +
+            "laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, " +
+            "sunt in culpa qui officia deserunt mollit anim id est laborum."
+    });
+});
+
+app.get('/article', function(req) {
+    return json({
+        title: "ARTICLE TITLE",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. FOR MORE ARTICLE LOGIN"
+    });
+});
+
 app.get( '/ping', function ( req ) {
 	var servletRequest = req.env.servletRequest;
 
