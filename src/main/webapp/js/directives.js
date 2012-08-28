fdaa987ad99bd9a4e83eeccae6e536a4904e8c7f
jameshines10 <jhines@pykl.com>
2012-08-28 14:45:57 -0400

    Added a very simple directive to embed a Google Docs viewer when given a src

--- src/main/webapp/js/directives.js
+++ src/main/webapp/js/directives.js
@@ -9,4 +9,13 @@ angular.module( 'bgc.directives', [] )
             restrict:'E',
             templateUrl:'lib/profile/partials/profileTemplate.html'
         }
-    }]);
+    }])
+    .directive('docViewer', function (){
+        return{
+            restrict:'E',
+            scope: {
+                src: '@'
+            },
+            template: '<iframe src={{src}} style="width:550px; height:730px;" frameborder="3px"></iframe>'
+        }
+    });
