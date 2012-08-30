1f80827cb061166a276011e6079f5eb93512ebd1
jameshines10 <jhines@pykl.com>
2012-08-30 09:25:12 -0400

    The DocViewer directive now accepts a "google-docs" attribute that has a value of true or false

--- src/main/webapp/js/directives.js
+++ src/main/webapp/js/directives.js
@@ -12,10 +12,21 @@ angular.module( 'bgc.directives', [] )
     }])
     .directive('docViewer', function (){
         return{
+            url: '',
             restrict:'E',
-            scope: {
-                src: '@'
-            },
-            template: '<iframe src={{src}} style="width:550px; height:730px;" frameborder="3px"></iframe>'
+            template: '<iframe src="" style="width:98%; height:730px;" frameborder="3px"></iframe>',
+            compile: function(element, attrs, transclude){
+                var isGoogleDoc = true;
+
+                if(attrs.googleDoc){
+                    isGoogleDoc = attrs.googleDoc == 'true' ? true : false;
+                }
+
+                if(isGoogleDoc){
+                    element.context.firstChild.src = attrs.url;
+                }else{
+                    element.context.firstChild.src = 'http://docs.google.com/viewer?url=' + attrs.url + '&embedded=true';
+                }
+            }
         }
     });
