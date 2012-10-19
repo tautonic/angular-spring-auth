234fb8b231b8ad29160838cbca18e61200ba026b
Todd Barchok <todd@barchok.com>
2012-10-19 15:02:55 -0400

    Fixed errors in links on logged in menu

--- src/main/webapp/lib/pyklsecurity/auth.js
+++ src/main/webapp/lib/pyklsecurity/auth.js
@@ -213,8 +213,8 @@
                 <thumbnail  type="profile" size="small" rotation="clockwise"  image="images/rosie.jpeg"/> \
             </a> \
         <ul class="dropdown-menu"> \
-            <li><span class="gradient"><a ng-href="#/profile/">My profile</a></span></li> \
-            <li><span class="gradient"><a ng-href="#/profile/">Change Picture</a></span></li> \
+            <li><span class="gradient"><a ng-href="#/profiles/view/{{auth.principal.id}}">My profile</a></span></li> \
+            <li><span class="gradient"><a ng-href="#/profiles/update/{{auth.principal.id}}">Change Picture</a></span></li> \
         <li ng-show="auth.isUserInRole(\'ROLE_ADMIN\')"><span class="gradient"><a ng-href="#/admin/">Admin Panel</a></span></li> \
             <li><span class="gradient"><a ng-click="signout()">Logout</a></span></li> \
         </ul> \
