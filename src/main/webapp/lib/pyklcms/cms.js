b61867ee56013eddb2fcd18039a8dd3fe6fd7eb1
jcook <jcook@pykl.com>
2012-10-24 11:18:37 -0400

    Reorganizing the cms-related angular components into their own library. Elaborated on the CMS popup editor.

--- src/main/webapp/lib/pyklcms/cms.js
+++ src/main/webapp/lib/pyklcms/cms.js
@@ -1,9 +1,76 @@
-var cmsCtrl;
+var pykl = window.pykl || {};
 
-(function() {
-    angular.module('pykl.cms', []);
+/**
+ * todo: Determine some naming conventions. The service, directive, events and controller are all
+ * over the board.
+ */
+(function(window, angular, undefined) {
+
+    'use strict';
+
+    var pyklCms = angular.module('pykl', []);
 
     var CMS_EDIT_EVENT = '$pykl.cms-edit';
+
+    /**
+     * @ngdoc object
+     * @name pykl.cms
+     * @requires $http
+     *
+     * @description
+     * A service which will query the server for CMS resources.
+     *
+     * @example
+     *
+     */
+    pyklCms.factory( 'pykl.$cms', ['$rootScope', '$http', '$log', function ( $rootScope, $http, $log ) {
+
+        /**
+         * Fetches a list of resources from the server based on the supplied key. The locale field is
+         * optional. If it is not supplied the result will contain all locales for the provided key.
+         * If it is supplied, the result will contain the "most appropriate" match for the provided
+         * locale. This does not imply an exact match, although it will be an exact match if the exact
+         * locale is found. If an exact locale is not found, the locale degradation rules will result
+         * in the "best" result being returned.
+         *
+         * Success callback is required, and will be invoked with a data argument which contains an
+         * array of the matching resources. If the optional error callback is provided, it will be
+         * invoked in the event of some server-side error. Note that an empty result will be returned
+         * to the success handler as an empty array.
+         *
+         * @param {String} key The lookup value for the resource. Can include an '@' symbol followed by
+         * locale as an alternate means of providing the locale to this function.
+         * @param {String} [locale] A combination of language and optional country code separated with
+         * an underscore. Language codes are specified by ISO-639 and country codes are in ISO-3166.
+         * @param {Function} success A callback function upon successful retrieval of resource. Will
+         * contain an array of results. No match found will return an empty array.
+         * @param {Function} [error] Called when there is a critical server error.
+         */
+        function getResources(key, locale, success, error) {
+            var args = Array.prototype.slice(arguments,0);
+            // First argument is the lookup key
+            key = args.shift();
+            // Second argument may be locale (string) or success (function)
+            locale = typeof args[0] === 'string' ? args.shift() : null;
+            success = args.shift();
+            // If there is a final argument, it is an error handler
+            error = args.shift();
+
+            // Create the URL to use in order to return the CMS content and the locale if forced
+            var parts = key.split('@');
+            var url = 'api/cms/' + parts[0];
+            var loc = parts[1] || locale;
+            if (parts.length > 1) url = url + '?locale=' + loc;
+
+            // Make a GET request to the server in order to perform the CMS lookup
+            $http({method: 'GET', url: url})
+                .success(updateScope).error(updateScope);
+        }
+
+        return {
+            getResources: getResources
+        }
+    }] );
 
     /**
      * Controller that supports the editing of CMS content on a page.
@@ -11,32 +78,39 @@ var cmsCtrl;
      * @param $scope
      * @param $http
      */
-    cmsCtrl = function($scope, $http) {
+    pykl.cmsCtrl = function($scope, $http) {
         var cms;
 
         $scope.modalShown = false;
         $scope.cms = {
             title: '',
-            description: '',
+            content: '',
             thumbnailUrl: ''
         };
 
         $scope.submit = function() {
-
-
             cms.title = 'Who\'s your Daddy!';
             $scope.modalShown = false;
             cms = null;
         };
 
+        $scope.cancel = function() {
+            console.log('Cancel pressed');
+            $scope.modalShown = false;
+            cms = null;
+        };
+
         $scope.$on(CMS_EDIT_EVENT, function(event, key, dispScope) {
-            cms = dispScope.cms;
+            $scope.cms = cms = dispScope.cms;
+
+            // Would love to know why this only works when I enclose it in scope.$apply(), but always throws a
+            // '$digest already in progress' error when I do so.
             $scope.$apply(function() {
                 $scope.modalShown = true;
             });
         });
     };
-    cmsCtrl.$inject = ['$scope', '$http'];
+    pykl.cmsCtrl.$inject = ['$scope', '$http'];
 
     /**
      * @ngdoc directive
@@ -50,7 +124,7 @@ var cmsCtrl;
      *
      * The 'at' symbol (@) can't be used as part of the key value because it is dedicated as a separator
      * character which the CMS property uses to differentiate the key from the locale.
-     * ( ie x-cms="<key>@<locale>" )
+     * ( ie x-cms="<key>@<locale>" or x-cms="footer@fr_CA" )
      *
      * @element ANY
      * @param {expression} Evaluates to the string key used to identify the CMS value. The
@@ -66,8 +140,8 @@ var cmsCtrl;
      <th x-cms="name-title"/>
      </example>
      */
-    angular.module('pykl.cms').directive('cms', ['$rootScope', '$http', '$auth',
-        function ($rootScope, $http, $auth) {
+    pyklCms.directive('cms', ['$rootScope', '$http', 'pykl.$cms', '$auth',
+        function ($rootScope, $http, $cms, $auth) {
             return {
                 // Is applied as an element's attribute
                 restrict: 'A',
@@ -109,32 +183,22 @@ var cmsCtrl;
                             $rootScope.$broadcast(CMS_EDIT_EVENT, key, scope);
                         });
 
-
-                        // Create the URL to use in order to return the CMS content and the locale if forced
-                        var parts = key.split('@');
-                        var url = 'api/cms/' + parts[0];
-                        if (parts.length > 1) url = url + '?locale=' + parts[1];
-
-                        // Make a GET request to the server in order to perform the CMS lookup
-                        $http({method: 'GET', url: url})
-                            .success(function (data, status, headers, config) {
-                                // If the HTTP response is an array and the first element contains a content
-                                // property, then we found what we were looking for.
-                                var contentFound = data.length && data[0] && data[0].content;
-                                if (contentFound) {
-                                    // Populate the HTML element with content
-                                    scope.cms = data[0];
-                                } else {
-                                    scope.cms = 'CMS CONTENT NOT FOUND!';
-                                }
-                            })
-                            .error(function (data, status, headers, config) {
+                        var updateScope = function(data) {
+                            // If the HTTP response is an array and the first element contains a content
+                            // property, then we found what we were looking for.
+                            var contentFound = data && data.length && data[0] && data[0].content;
+                            if (contentFound) {
+                                // Populate the HTML element with content
+                                scope.cms = data[0];
+                            } else {
                                 scope.cms = 'CMS CONTENT NOT FOUND!';
-                            });
+                            }
+                        };
+
+                        $cms.getResources(key, updateScope);
                     }
                 }
             }
         }
     ]);
-})();
-
+})(window, window.angular, undefined);
