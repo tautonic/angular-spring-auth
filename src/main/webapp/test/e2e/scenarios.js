6f75d9a1b32382355eb4955a831a344be4402967
Todd Barchok <todd@barchok.com>
2012-12-10 13:45:19 -0500

    End to end testing, nearly done

--- src/main/webapp/test/e2e/scenarios.js
+++ src/main/webapp/test/e2e/scenarios.js
@@ -4,42 +4,42 @@
 
 describe('my app', function() {
 
-  beforeEach(function() {
-    browser().navigateTo('../../app/index.html');
-  });
-
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser().location().url()).toBe("/view1");
-  });
-
-
-  describe('view1', function() {
-
     beforeEach(function() {
-      browser().navigateTo('#/view1');
+        browser().navigateTo('../../');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser().navigateTo('#/view2');
+    it('should automatically redirect to homepage when location hash/fragment is empty', function() {
+        expect(browser().location().url()).toBe("/");
     });
 
 
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element('[ng-view] p:first').text()).
-        toMatch(/partial for view 2/);
+    describe('homepage', function() {
+
+        beforeEach(function() {
+            browser().navigateTo('#/home');
+        });
+
+
+        it('should be three articles displayed as news items', function() {
+            expect(repeater('.articles .article').count()).
+            toEqual(4); //technically there's four items because the "no news found" is being displayed
+        });
+
     });
 
-  });
+
+    describe('login', function() {
+
+        beforeEach(function() {
+            browser().navigateTo('#/login');
+        });
+
+
+        it('should render login view when user navigates to /login', function() {
+            expect(element('form[name=login]').count()).
+            toEqual(1);
+        });
+
+    });
 });
