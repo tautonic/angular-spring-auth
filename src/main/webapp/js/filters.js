060920f6012479982839930030b97a4a59c20f0e
jameshines10 <jhines@pykl.com>
2012-12-24 23:11:21 -0500

    Added a filter for shortening the file name of file attachments 	modified:   src/main/webapp/js/filters.js 	modified:   src/main/webapp/lib/pyklresource/partials/view.html

--- src/main/webapp/js/filters.js
+++ src/main/webapp/js/filters.js
@@ -74,3 +74,32 @@ angular.module('bgc.filters').filter('notags', function(){
         return text;
     }
 });
+
+angular.module('bgc.filters').filter('shorten', function(){
+    return function(string, length){
+        // only run when the substr function is broken
+        if ('ab'.substr(-1) != 'b'){
+            /**
+             *  Get the substring of a string
+             *  @param  {integer}  start   where to start the substring
+             *  @param  {integer}  length  how many characters to return
+             *  @return {string}
+             */
+            String.prototype.substr = function(substr) {
+                return function(start, length) {
+                    // did we get a negative start, calculate how much it is from the beginning of the string
+                    if (start < 0) start = this.length + start;
+
+                    // call the original function
+                    return substr.call(this, start, length);
+                }
+            }(String.prototype.substr);
+        }
+
+        if(string.length > 22){
+            return string.substr(0, 11) + '...' + string.substr(-11);
+        }
+
+        return string;
+    }
+});
